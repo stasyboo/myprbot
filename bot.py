4f7adc34e370371740e94cb823d46549bcf4de3e
stasyboo <b.stasy@yahoo.com>
2025-04-30 20:17:27 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -101,16 +101,9 @@ async def main():
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, message_handler))
 
     await app.initialize()
-    await app.start()
-    await app.bot.set_webhook(
-        url=f"https://{RENDER_EXTERNAL_HOSTNAME}/telegram",
-        drop_pending_updates=True
-    )
-    await app.run_webhook(
-        listen="0.0.0.0",
-        port=PORT,
-        webhook_path="/telegram"
-    )
+    await app.run_polling() 
+
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    import asyncio
+    asyncio.run(app.run_polling())
